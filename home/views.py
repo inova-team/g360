7a964368476af07d2166093d86b09d6d70c6e56a
Alonso <a20170809@pucp.edu.pe>
2021-08-09 02:35:32 -0500

    Avance del slider - home

--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def renderHome(request):
-    return render(request, 'home/home.html')
+    context = {
+        'title_page' : 'Home',
+    }
+    return render(request, 'home/home.html', context)
