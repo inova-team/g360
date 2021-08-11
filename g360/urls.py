5a1c3e749560a3f964102dd63c61869ce92f3600
DESKTOP-M7K6180\Usuario <gilmer.cabrera@pucp.edu.pe>
2021-08-11 13:55:49 -0500

    agregar urls - g360

--- g360/urls.py
+++ g360/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('authentication/',include('authentication.urls'))
+    path('dictionary', include('dictionary.urls')),
 ]
 
 if settings.DEBUG:
