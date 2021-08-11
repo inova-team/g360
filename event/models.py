8fdf49b42bbc52942f2221218d20c57cfb0a1376
Alonso <a20170809@pucp.edu.pe>
2021-08-11 17:46:37 -0500

    Creacion Models Event

--- event/models.py
+++ event/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+from authentication.models import Staff
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=1000)
+    description = models.TextField(max_length=5000)
+    user_register = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name="event_user_register")
+    user_last_update = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name="event_user_last_update")
+    date_register = models.DateTimeField(editable=False)
+    date_last_update = models.DateTimeField(editable=True, null=True)
+    responsable = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name="event_responsable")
+    banner = models.ImageField(upload_to='article/banner/', null=True, blank=True)
+    link_event = models.CharField(max_length=1000)
+    link_form = models.CharField(max_length=500)
+    number_participants = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def delete(self, *args, **kwargs):
+        self.banner.delete()
+        super().delete(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_register = timezone.now()
+            self.date_last_update= timezone.now()
+        return super().save(*args, **kwargs)
