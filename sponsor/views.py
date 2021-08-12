544bff3a13795bad9332b49d8877857e68d61dc6
LAPTOP-BL4CHF1T\juanc <jcasasz@pucp.edu.pe>
2021-08-12 03:04:01 -0500

    Actualizar sponsor - sponsor

--- sponsor/views.py
+++ sponsor/views.py
@@ -49,6 +49,35 @@ def sponsor_upload(request):
     }
     return render(request, 'sponsor/sponsor_upload.html', context)
 
+def sponsor_update(request, pk):
+    users = User.objects.all()
+    sponsor = Sponsor.objects.get(pk=pk)
+    if request.POST:
+        if request.POST.get('author_select') != "0":
+            author_select = User.objects.get(pk=request.POST.get('author_select'))
+            sponsor.author = author_select
+
+        sponsor.name = request.POST.get('name')
+        sponsor.description = request.POST.get('description')
+        sponsor.type = request.POST.get('type')
+
+        sponsor.fb_link = request.POST.get('fb_link')
+        sponsor.instagram_link = request.POST.get('instagram_link')
+        sponsor.twitter_link = request.POST.get('twitter_link')
+        sponsor.website_link = request.POST.get('website_link')
+
+        if request.FILES.get('image_uploads'):
+            sponsor.banner = request.FILES.get('image_uploads')
+        sponsor.save()
+
+        return redirect('sponsor_list')
+    context = {
+        'sponsor': sponsor,
+        'users': users,
+        'title_page': 'Actualización de Sponsor'
+    }
+    return render(request, 'sponsor/sponsor_update.html', context)
+
 def sponsor_delete(request, pk):
     sponsor = Sponsor.objects.get(pk=pk)
     sponsor.delete()
